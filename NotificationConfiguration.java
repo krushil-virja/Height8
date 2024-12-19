605bb4cebb20ba895791e73c935968245acb4557
krushil <krushil.v@h8ssrms.com>
2024-12-19 10:59:17 +0530

    testing is work properly

--- NotificationConfiguration.java
+++ NotificationConfiguration.java
@@ -3,39 +3,94 @@ public class NotificationConfiguration {
    private int id;
    private String level;
    private String event;
-   private NotificationConfiguration endpoints;
+private String test;  
+
+
+
+
+
+
+
+
 
 public int getId() {
     return id;
 }
 
+
+
+
+
+
+
+
+
+
 public void setId(int id) {
     this.id = id;
 }
+
+
+
+
+
+
 
 public String getLevel() {
     return level;
 }
 
+
+
+
+
+
+
+
+
+
+
+
+
 public void setLevel(String level) {
     this.level = level;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 public String getEvent() {
     return event;
 }
 
+
+
+
+
+
+
+
+
+
 public void setEvent(String event) {
     this.event = event;
 }
 
-public NotificationConfiguration getEndpoints() {
-    return endpoints;
-}
-
-public void setEndpoints(NotificationConfiguration endpoints) {
-    this.endpoints = endpoints;
-}
 
 
 }
